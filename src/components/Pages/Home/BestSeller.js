5216d4c63e566212bd97cd2a124a91a105eefc81
himelislam <himel7100@gmail.com>
2022-05-22 01:16:11 +0600

    home page best seller products added

---
+++ src/components/Pages/Home/BestSeller.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
+
+const BestSeller = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=> {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
+    return (
+        <div className='px-10 my-20'>
+            <div className='text-center w-[500px] mx-auto mb-10'>
+                <h2 className='font-semibold text-3xl my-2'>BEST SELLER</h2>
+                <p className='font-light mb-4'>All best seller product are now available for you and your can buy this product from here any time any where so sop now</p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                {
+                    products.slice(0,4).map(product => <Product product={product}></Product>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default BestSeller;
---
+++ src/components/Pages/Home/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Product = ({product}) => {
+    return (
+        <div class="card  bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src={product.img} alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="font-semibold text-xl">{product.name}</h2>
+                <p className='font-light mb-[-10px]'><small>{product.description.slice(0,100)}</small></p>
+                <p><small>Minimum Order Quantity:{product.minimunQuantity}</small></p>
+                <p><small>Available Quantity: {product.availableQuantity}</small></p>
+                <div class="card-actions">
+                    <button class="btn btn-primary">BOOK NOW</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
