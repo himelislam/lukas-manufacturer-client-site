f986ad5709ac88e6cad02a7b5d6b5d21d808ae92
himelislam <himel7100@gmail.com>
2022-06-06 01:43:42 +0600

    transfered

--- src/components/Pages/Home/Banner.js
+++ src/components/Pages/Home/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Banner = () => {
+const  Banner = () => {
     return (
         <div>
             <div className="carousel w-full">
@@ -10,7 +10,7 @@ const Banner = () => {
                             <a href="#slide4" className="btn btn-circle">❮</a>
                             <div className='text-center'>
                                 <h3 className='text-white font-bold text-xl lg:text-2xl mb-2'>NEW TECHNOLOGY & BUILD</h3>
-                                <h1 className='text-white font-bold text-2xl lg:text-5xl mb-3'>WHEELS & PARTS COLLECTION</h1>
+                                <h1 className='text-white font-bold text-2xl lg:text-5xl mb-3'>WHEELS & PARTS COLLECTION </h1>
                                 <button className='btn btn-secondary btn-sm text-white'>Book Now</button>                               
                             </div>
                             <a href="#slide2" className="btn btn-circle">❯</a>
